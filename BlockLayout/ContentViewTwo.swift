4b0c062412d9c31934775ecc429bebf6ecc19beb
Ray Huang <rhuang@lcs.on.ca>
2026-02-07 10:22:19 -0500

    Completed my second layout

---
+++ BlockLayout/ContentViewTwo.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewTwo.swift
+//  BlockLayout
+//
+//  Created by Panyui Huang on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentViewTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewTwo()
+}
