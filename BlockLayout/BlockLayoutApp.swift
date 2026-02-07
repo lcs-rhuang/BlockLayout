2441bd29dff5cc80d2475afe6809e3fa9126130f
Ray Huang <rhuang@lcs.on.ca>
2026-02-07 09:55:53 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Panyui Huang on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
